2dae9956d8d285b77aa5415c3bfeacb92c4da952
Neil <pppp29654213@gmail.com>
2015-02-20 08:43:22 +0000

    Exercise in ch5

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -14,4 +14,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
   end
+  
+  test "sign up page" do
+    get signup_path
+    assert_template 'users/new'
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
+  end
 end
